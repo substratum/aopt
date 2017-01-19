0a8419cb843a2b37b4e68ee26e0c948767db1823
Surge1223 <surge1223@gmail.com>
2017-01-19 01:34:00 -0600

    Allow both host and target builds and use static libraries to avoid AOSP runtime dependencies

--- Android.mk
+++ Android.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2014 The Android Open Source Project
+# Copyright (C) 2017 The Android Open Source Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -15,7 +15,6 @@
 #
 
 # This tool is prebuilt if we're doing an app-only build.
-ifeq ($(TARGET_BUILD_APPS)$(filter true,$(TARGET_BUILD_PDK)),)
 
 # ==========================================================
 # Setup some common variables for the different build
@@ -45,9 +44,147 @@ aoptSources := \
     WorkQueue.cpp \
     XMLNode.cpp \
     ZipEntry.cpp \
-    ZipFile.cpp
+    ZipFile.cpp \
+    logstubs.cpp
 
 aoptTests := \
+    tests/AoptConfig_test.cpp \
+    tests/AoptGroupEntry_test.cpp \
+    tests/Pseudolocales_test.cpp \
+    tests/ResourceFilter_test.cpp \
+    tests/ResourceTable_test.cpp
+
+aoptStaticLibs := \
+    libandroidfw-static \
+    libpng \
+    libexpat_static \
+    libutils \
+    libcutils \
+    libziparchive \
+    liblog \
+    libbase \
+    libm \
+    libc \
+    libz \
+    libc++_static
+
+CFLAGS := \
+	-DHAVE_SYS_UIO_H \
+	-DHAVE_PTHREADS \
+	-DHAVE_SCHED_H \
+	-DHAVE_IOCTL \
+	-DHAVE_TM_GMTOFF \
+	-DANDROID_SMP=1  \
+	-DHAVE_ENDIAN_H \
+	-DHAVE_POSIX_FILEMAP \
+	-DHAVE_OFF64_T \
+	-DHAVE_LITTLE_ENDIAN_H \
+	-D__ANDROID__ \
+	-DHAVE_ANDROID_OS=1 \
+	-D_ANDROID_CONFIG_H \
+	-DHAVE_ERRNO_H='1' \
+	-DSTATIC_ANDROIDFW_FOR_TOOLS \
+
+aoptcppFlags := \
+		-std=gnu++1y \
+		-Wno-missing-field-initializers \
+		-fno-exceptions -fno-rtti -Os
+
+aoptCflags := -D'AOPT_VERSION="android-$(PLATFORM_VERSION)-$(TARGET_BUILD_VARIANT)"'
+aoptCflags += -Wno-format-y2k
+aoptCflags += $(CFLAGS)
+
+aoptLdLibs := -lc -lgcc -ldl -lz -lm
+
+aoptIncludes := \
+        $(LOCAL_PATH)/include \
+        system/core/base/include \
+        system/core/libutils \
+        system/core/liblog \
+        system/core/libcutils \
+        $(LOCAL_PATH)/libpng \
+        external/expat \
+        external/zlib \
+        external/libcxx/include \
+        system/core/libziparchive \
+        frameworks/base/libs/androidfw \
+        frameworks/base/include/androidfw
+
+# ==========================================================
+# Build the target static library: libandroidfw-static
+# ==========================================================
+include $(CLEAR_VARS)
+
+ANDROIDFW_PATH := ../../libs/androidfw
+LOCAL_C_INCLUDES := $(LOCAL_PATH)/
+LOCAL_EXPORT_C_INCLUDES := $(LOCAL_C_INCLUDES)
+
+androidfw_srcs := \
+    $(ANDROIDFW_PATH)/Asset.cpp \
+    $(ANDROIDFW_PATH)/AssetDir.cpp \
+    $(ANDROIDFW_PATH)/AssetManager.cpp \
+    $(ANDROIDFW_PATH)/LocaleData.cpp \
+    $(ANDROIDFW_PATH)/misc.cpp \
+    $(ANDROIDFW_PATH)/ObbFile.cpp \
+    $(ANDROIDFW_PATH)/ResourceTypes.cpp \
+    $(ANDROIDFW_PATH)/StreamingZipInflater.cpp \
+    $(ANDROIDFW_PATH)/TypeWrappers.cpp \
+    $(ANDROIDFW_PATH)/ZipFileRO.cpp \
+    $(ANDROIDFW_PATH)/ZipUtils.cpp \
+    $(ANDROIDFW_PATH)/BackupData.cpp \
+    $(ANDROIDFW_PATH)/BackupHelpers.cpp \
+    $(ANDROIDFW_PATH)/CursorWindow.cpp \
+    $(ANDROIDFW_PATH)/DisplayEventDispatcher.cpp
+
+LOCAL_MODULE:= libandroidfw-static
+LOCAL_MODULE_TAGS := optional
+LOCAL_CFLAGS :=  $(aoptCflags)
+LOCAL_CPPFLAGS := $(aoptCppFlags)
+LOCAL_C_INCLUDES := external/zlib
+LOCAL_STATIC_LIBRARIES := libziparchive libbase
+LOCAL_SRC_FILES := $(androidfw_srcs)
+include $(BUILD_STATIC_LIBRARY)
+
+
+# ==========================================================
+# Build the target executable: aopt
+# ==========================================================
+include $(CLEAR_VARS)
+
+LOCAL_CFLAGS :=  $(aoptCflags)
+LOCAL_CPPFLAGS := $(aoptCppFlags)
+LOCAL_C_INCLUDES := $(aoptIncludes)
+LOCAL_STATIC_LIBRARIES := $(aoptStaticLibs)
+LOCAL_LDLIBS := $(aoptLdLibs)
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := EXECUTABLES
+LOCAL_SRC_FILES := $(aoptMain) $(aoptSources)
+
+LOCAL_UNSTRIPPED_PATH := $(PRODUCT_OUT)/symbols/utilities
+LOCAL_LDFLAGS += -static
+LOCAL_FORCE_STATIC_EXECUTABLE := true
+LOCAL_PACK_MODULE_RELOCATIONS := false
+LOCAL_ADDITIONAL_DEPENDENCIES := $(LOCAL_PATH)/Android.mk
+
+
+LOCAL_MODULE := aopt
+
+LOCAL_MODULE_STEM_32 := aopt
+LOCAL_MODULE_STEM_64 := aopt64
+LOCAL_MODULE_PATH_32 := $(ANDROID_PRODUCT_OUT)/system/bin
+LOCAL_MODULE_PATH_64 := $(ANDROID_PRODUCT_OUT)/system/bin
+LOCAL_MULTILIB := both
+
+include $(BUILD_EXECUTABLE)
+
+
+# ==========================================================
+# Setup some common variables for the different build
+# targets here.
+# ==========================================================
+
+include $(CLEAR_VARS)
+aoptHostTests := \
     tests/AoptConfig_test.cpp \
     tests/AoptGroupEntry_test.cpp \
     tests/Pseudolocales_test.cpp \
@@ -64,8 +201,8 @@ aoptHostStaticLibs := \
     libziparchive-host \
     libbase
 
-aoptCFlags := -DAOPT_VERSION=\"$(BUILD_NUMBER_FROM_FILE)\"
-aoptCFlags += -Wall -Werror
+aoptHostCFlags := -D'AOPT_VERSION="android-$(PLATFORM_VERSION)-$(TARGET_BUILD_VARIANT)"'
+aoptHostCFlags += -Wall -Werror
 
 aoptHostLdLibs_linux := -lrt -ldl -lpthread
 
@@ -83,8 +220,8 @@ include $(CLEAR_VARS)
 
 LOCAL_MODULE := libaopt
 LOCAL_MODULE_HOST_OS := darwin linux windows
-LOCAL_CFLAGS := -Wno-format-y2k -DSTATIC_ANDROIDFW_FOR_TOOLS $(aoptCFlags)
-LOCAL_CPPFLAGS := $(aoptCppFlags)
+LOCAL_CFLAGS := -Wno-format-y2k -DSTATIC_ANDROIDFW_FOR_TOOLS $(aoptHostCFlags)
+LOCAL_CPPFLAGS := $(aoptHostCppFlags)
 LOCAL_CFLAGS_darwin := -D_DARWIN_UNLIMITED_STREAMS
 LOCAL_SRC_FILES := $(aoptSources)
 LOCAL_STATIC_LIBRARIES := $(aoptHostStaticLibs)
@@ -99,8 +236,8 @@ include $(CLEAR_VARS)
 
 LOCAL_MODULE := aopt
 LOCAL_MODULE_HOST_OS := darwin linux windows
-LOCAL_CFLAGS := $(aoptCFlags)
-LOCAL_CPPFLAGS := $(aoptCppFlags)
+LOCAL_CFLAGS := $(aoptHostCFlags)
+LOCAL_CPPFLAGS := $(aoptHostCppFlags)
 LOCAL_LDLIBS_darwin := $(aoptHostLdLibs_darwin)
 LOCAL_LDLIBS_linux := $(aoptHostLdLibs_linux)
 LOCAL_SRC_FILES := $(aoptMain)
@@ -116,8 +253,8 @@ include $(BUILD_HOST_EXECUTABLE)
 include $(CLEAR_VARS)
 
 LOCAL_MODULE := libaopt_tests
-LOCAL_CFLAGS := $(aoptCFlags)
-LOCAL_CPPFLAGS := $(aoptCppFlags)
+LOCAL_CFLAGS := $(aoptHostCFlags)
+LOCAL_CPPFLAGS := $(aoptHostCppFlags)
 LOCAL_LDLIBS_darwin := $(aoptHostLdLibs_darwin)
 LOCAL_LDLIBS_linux := $(aoptHostLdLibs_linux)
 LOCAL_SRC_FILES := $(aoptTests)
@@ -126,6 +263,3 @@ LOCAL_STATIC_LIBRARIES := libaopt $(aoptHostStaticLibs)
 LOCAL_STATIC_LIBRARIES_windows := $(aoptHostStaticLibs_windows)
 
 include $(BUILD_HOST_NATIVE_TEST)
-
-
-endif # No TARGET_BUILD_APPS or TARGET_BUILD_PDK
